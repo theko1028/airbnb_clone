fa270dc85a582ca7c70b82979ba42def09bdc1f1
root <root@DESKTOP-THEKO.localdomain>
2020-05-15 01:32:54 +0900

    Edit CustomUserAdmin class  in admin.py
    Change parent class from admin.ModelAdmin to UserAdmin and
    Overwrite fieldsets

--- users/admin.py
+++ users/admin.py
@@ -1,11 +1,24 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 from . import models
 
 
 @admin.register(models.User)
-class CustomUserAdmin(admin.ModelAdmin):
+class CustomUserAdmin(UserAdmin):
 
-    """ Custom User Admin """
-
-    list_display = ("username", "email", "gender", "language", "currency", "superhost")
-    list_filter = ("language", "currency", "superhost")
+    fieldsets = UserAdmin.fieldsets + (
+        (
+            "Custom Profiles",
+            {
+                "fields": (
+                    "avatar",
+                    "gender",
+                    "bio",
+                    "birthdate",
+                    "language",
+                    "currency",
+                    "superhost",
+                )
+            },
+        ),
+    )
