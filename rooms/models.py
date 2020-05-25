9cf13f781a3b04402934f90765dbfaee55412f68
root <root@DESKTOP-THEKO.localdomain>
2020-05-25 22:43:52 +0900

    Add amenities and facilities with codes

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from django_countries.fields import CountryField
 from core import models as core_models
-from users import models as users_models
+from users import models as user_models
 
 
 class AbstractItem(core_models.TimeStampedModel):
