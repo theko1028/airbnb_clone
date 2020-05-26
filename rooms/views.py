cd90d053e017c8b5635fbaee0dd3be729b0db4e9
root <root@DESKTOP-THEKO.localdomain>
2020-05-26 23:04:44 +0900

    Create home view

--- rooms/views.py
+++ rooms/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.paginator import Paginator, EmptyPage
+from . import models
 
-# Create your views here.
+
+def all_rooms(request):
+    page = request.GET.get("page", 1)
+    room_list = models.Room.objects.all()
+    paginator = Paginator(room_list, 5, orphans=2)
+    try:
+        rooms = paginator.page(int(page))
+        return render(request, "rooms/home.html", {"rooms": rooms})
+    except EmptyPage:
+        return redirect("/")
