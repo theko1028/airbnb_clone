2bcbd54c0e0865f348a903128ffbe7b21b91745a
root <root@DESKTOP-THEKO.localdomain>
2020-05-26 23:55:49 +0900

    Edit home view with ListView

--- rooms/views.py
+++ rooms/views.py
@@ -1,14 +1,20 @@
-from django.shortcuts import render, redirect
-from django.core.paginator import Paginator, EmptyPage
+from django.utils import timezone
+from django.views.generic import ListView
 from . import models
 
 
-def all_rooms(request):
-    page = request.GET.get("page", 1)
-    room_list = models.Room.objects.all()
-    paginator = Paginator(room_list, 5, orphans=2)
-    try:
-        rooms = paginator.page(int(page))
-        return render(request, "rooms/home.html", {"rooms": rooms})
-    except EmptyPage:
-        return redirect("/")
+class HomeView(ListView):
+
+    """ HomeView Definition """
+
+    model = models.Room
+    paginate_by = 5
+    paginate_orphans = 2
+    ordering = "price"
+    context_object_name = "rooms"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        now = timezone.now()
+        context["now"] = now
+        return context
